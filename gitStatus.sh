ed1a80fde34314263f5e2f5992fd484869f9c708
parthshah889900 <parthshah8899002gmail.com>
2021-12-10 12:27:16 +0530

    adding

--- gitStatus.sh
+++ gitStatus.sh
@@ -19,5 +19,5 @@ echo "Number of file copying:"${#myArray[@]}
 for (( i = 0 ; i < ${#myArray[@]} ; i++))
 do
   echo ${myArray[$i]}
-  cp ${myArray[$i]} reports/
+  cp --parent ${myArray[$i]} reports/
 done
