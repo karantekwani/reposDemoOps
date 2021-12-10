fded4c357de12cf430595d3e193b2b46f15644ad
parthshah889900 <parthshah8899002gmail.com>
2021-12-10 12:23:23 +0530

    adding

---
+++ gitStatus.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd /home/ubuntu/repo/demoRepo
+lastServerCommit=$(git rev-parse HEAD)
+git stash
+git pull
+lastGitCommit=$(git rev-parse HEAD)
+echo $lastGitCommit
+echo $lastServerCommit
+echo "\n"
+echo "Files that are changes between two commits:"
+echo "\n"
+git diff --name-only $lastGitCommit $lastServerCommit
+git diff --name-only $lastGitCommit $lastServerCommit -- '*.php' > report6.txt
+filename=report6.txt
+declare -a myArray
+myArray=(`cat "$filename"`)
+echo "${myArray[0]}"
+echo "Number of file copying:"${#myArray[@]}
+for (( i = 0 ; i < ${#myArray[@]} ; i++))
+do
+  echo ${myArray[$i]}
+  cp ${myArray[$i]} reports/
+done
